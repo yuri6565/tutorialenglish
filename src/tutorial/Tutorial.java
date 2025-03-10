23a6a90560065a50c8e7dc29e7a4508064159f06
yuri6565 <p6891657@gmail.com>
2025-03-10 07:49:21 -0500

    prueba rama

--- src/tutorial/Tutorial.java
+++ src/tutorial/Tutorial.java
@@ -15,6 +15,7 @@ public class Tutorial {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hola prueba ingles");
     }
     
 }
